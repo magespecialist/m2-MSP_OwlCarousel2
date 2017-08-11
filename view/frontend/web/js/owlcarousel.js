e47f732ea525c8ed73c5989758648307ff3b6aca
Lorenzo Stramaccia <lorenzo.stramaccia@magespecialist.it>
2017-08-11 10:06:00 +0200

    Rollback to 2.2.1 + backport fix infinite loop

--- view/frontend/web/js/owlcarousel.js
+++ view/frontend/web/js/owlcarousel.js
@@ -22,7 +22,7 @@ define([
     "OwlCarousel-min"
 ], function($){
     return function (config, elem) {
-	$(elem).addClass('owl-carousel');
+        $(elem).addClass('owl-carousel');
         return $(elem).owlCarousel(config);
     }
 });
